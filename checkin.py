09fe512bf20eef264aa475aba0c7d540f0e0d9a4
a11b2 <breaven9285955@gmail.com>
2026-01-19 12:49:44 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     if cookies[0] != "":
 
         check_in_url = "https://glados.cloud/api/user/checkin"        # 签到地址
-        status_url = "https://glados.space/api/user/status"          # 查看账户状态
+        status_url = "https://glados.cloud/api/user/checkin"          # 查看账户状态
 
         referer = 'https://glados.cloud/api/user/checkin'
         origin = "https://glados.cloud"
@@ -95,5 +95,6 @@ if __name__ == '__main__':
     else:
         pushdeer = PushDeer(pushkey=sckey) 
         pushdeer.send_text(title, desp=context)
+
 
 

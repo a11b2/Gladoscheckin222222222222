cfa4cdaa268b212052c5a9d417920dedcf24fd51
a11b2 <breaven9285955@gmail.com>
2026-01-19 12:43:46 +0800

    Update check-in URLs and improve logging

--- checkin.py
+++ checkin.py
@@ -20,10 +20,10 @@ if __name__ == '__main__':
     cookies = os.environ.get("COOKIES", []).split("&")
     if cookies[0] != "":
 
-        check_in_url = "https://glados.space/api/user/checkin"        # 签到地址
+        check_in_url = "https://glados.cloud/console/checkin"        # 签到地址
         status_url = "https://glados.space/api/user/status"          # 查看账户状态
 
-        referer = 'https://glados.space/console/checkin'
+        referer = 'https://glados.cloud/console/checkin'
         origin = "https://glados.space"
         useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
         payload = {
@@ -95,3 +95,4 @@ if __name__ == '__main__':
     else:
         pushdeer = PushDeer(pushkey=sckey) 
         pushdeer.send_text(title, desp=context)
+
